055a2cb968afa947d1385d4e56410d287177da02
eriksson.e.erik@gmail.com <eriksson.e.erik@gmail.com>
2016-11-29 11:06:44 +0100

    added script for generating procedural bark texture using the included noise lib

---
+++ bARk/Assets/Scripts/ProceduralBark.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralBark : MonoBehaviour {
+    public float freq, lacunarity, gain;
+
+    public void GenerateTexture() {
+        // Setup noise
+        var noiseGenerator = new FastNoise(1234);
+        noiseGenerator.SetFractalOctaves(4);
+        noiseGenerator.SetFrequency(freq);
+        noiseGenerator.SetFractalLacunarity(lacunarity);
+        noiseGenerator.SetFractalGain(gain);
+
+        // Create texture and fill the pixels
+        var texture = new Texture2D(128, 256, TextureFormat.ARGB32, false);
+
+        for(int y = 0; y < texture.height; y++) {
+            for(int x = 0; x < texture.width; x++) {
+                texture.SetPixel(x, y, Color.Lerp(Color.black, Color.white, Mathf.InverseLerp(-1, 1, noiseGenerator.GetValueFractal(x, y))));
+                //texture.SetPixel(x, y, Color.Lerp(Color.black, Color.white, noiseGenerator.GetValue(x, y)));
+            }
+        }
+
+        // Apply all SetPixel calls
+        texture.Apply();
+
+        // connect texture to material of GameObject this script is attached to
+        GetComponent<Renderer>().sharedMaterial.mainTexture = texture;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
