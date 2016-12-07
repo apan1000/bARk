b37184e07a661ccbb7f7bd4c2ce0e0bfb8489f18
Ahlisen <ahlisen93@gmail.com>
2016-12-07 21:58:23 +0100

    Found a tree generator online and created a realistic growth function within

---
+++ bARk/Assets/Wasabimole/ProceduralTree/Changer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Wasabimole;
+
+
+public class Changer : MonoBehaviour {
+
+	Wasabimole.ProceduralTree.ProceduralTree tree;
+	float timeBetween;
+
+	// Use this for initialization
+	void Start () {
+		tree = GetComponent<Wasabimole.ProceduralTree.ProceduralTree>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > timeBetween && tree.GrowthMultiplier < 1) {
+			tree.GrowthMultiplier += 0.01f;
+			timeBetween = Time.time+0.02f;
+		}
+	}
+}
