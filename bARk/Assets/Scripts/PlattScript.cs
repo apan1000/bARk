e9ff58a95226b517d543ed60e7bc654e8a963a82
whitefenix <samuel.ekne@gmail.com>
2016-12-12 18:34:13 +0100

    Plättar!

---
+++ bARk/Assets/Scripts/PlattScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlattScript : MonoBehaviour {
+
+    public float growthtime = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        Spawn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.localScale.x < 1.0f)
+        {
+        //    transform.localScale = new Vector3(scale, scale/1000, scale);
+            Vector3 scale = new Vector3(Time.deltaTime * growthtime, Time.deltaTime * growthtime/100, Time.deltaTime * growthtime);
+            transform.localScale += scale;
+        }
+	}
+
+    public void Spawn()
+    {
+        transform.localScale = Vector3.zero;
+    }
+}
