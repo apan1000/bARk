2d98cacf2619af44a892261edaa1dfd69a52f116
apan1000 <fredrik.911225@gmail.com>
2017-01-09 19:47:52 +0100

    Id and class changes

--- assets/js/hallooou.js
+++ assets/js/hallooou.js
@@ -8,11 +8,11 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    if ($('#navbar').offset().top > 50) {
+        $("#navbar").addClass("top-nav-collapse");
         $(".scroll-top").fadeIn('1000', "easeInOutExpo");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $("#navbar").removeClass("top-nav-collapse");
         $(".scroll-top").fadeOut('1000', "easeInOutExpo");
     }
 });
